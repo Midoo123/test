20cac1550f2fd134546dc71691acbc7e40afd11d
Midoo123 <khalifamohamed50@gmail.com>
2020-10-17 21:59:43 +0200

    new code

---
+++ code.java
@@ -0,0 +1,12 @@
+ Volume in drive C has no label.
+ Volume Serial Number is 8C2F-C5FA
+
+ Directory of C:\Users\Med\Desktop\git
+
+17/10/2020  21:59    <DIR>          .
+17/10/2020  21:59    <DIR>          ..
+17/10/2020  21:59                 0 code.java
+17/10/2020  20:34               339 test1.txt
+17/10/2020  21:53               386 test2.txt
+               3 File(s)            725 bytes
+               2 Dir(s)  38ÿ516ÿ953ÿ088 bytes free
